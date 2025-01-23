e8efeadb0f2e3d522b6fbdca7ab9253d177071b8
Bintang <bintangananda405@gmail.com>
2025-01-23 11:22:17 +0700

    laravel blade template

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,15 @@ Route::get('/', [DashboardController::class, 'index']);
 Route::get('/form', [AuthController::class, 'register']);
 
 Route::post('/welcome', [AuthController::class, 'submit_form']);
+
+Route::get('/master', function () {
+    return view('layout/master');
+});
+
+Route::get('/table', function () {
+    return view('pages/table');
+});
+
+Route::get('/data-table', function () {
+    return view('pages/data-table');
+});
